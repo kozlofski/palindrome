5ea8e6f5186a71fc29a9f682b7f2d9536a7709ac
kozlofski <kozlofski.dev@gmail.com>
2020-05-11 14:12:25 +0200

    Fix case ignoring option

--- palindrome.cpp
+++ palindrome.cpp
@@ -16,7 +16,8 @@ int main() {
         "pp",
         "kobyla ma maly bok",
         "kobyla  ma maly bok",
-        "Kobyla  ma maly bok"
+        "Kobyla  ma maly bok",
+        "kOb yl A mA maly bo K"
     };
 
     for(std::string s : tests) {
@@ -32,11 +33,12 @@ bool isPalindrome(const std::string & str) {
         return false;
     }
 
-    int i = 0, j = str.length() - 1, caseModL = 0, caseModR = 0;
+    int i = 0, j = str.length() - 1;
+    int caseModL = 0, caseModR = 0;
 
     while(i < j) {
         (str[i] >= 'A' && str[i] <= 'Z') ? caseModL = 32 : caseModL = 0;
-        (str[j] >= 'A' && str[j] <= 'Z') ? caseModR = -32 : caseModR = 0;
+        (str[j] >= 'A' && str[j] <= 'Z') ? caseModR = 32 : caseModR = 0;
 
         if((str[i] + caseModL) != str[j] + caseModR) {
             std::cout << "\"" << str << 
